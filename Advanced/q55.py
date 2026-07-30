49519c6cae6c977929f844f86d1bac29e66c2387
Castillo Shaji <castilloshaji2005@gmail.com>
2026-07-30 23:47:57 +0530

    Add files via upload

---
+++ Advanced/q55.py
@@ -0,0 +1,44 @@
+#51. N-Queens
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+
+        board = [['.'] * n for _ in range(n)]
+
+        cols = set()
+        diag1 = set()
+        diag2 = set()
+
+        def backtrack(row):
+
+            if row == n:
+                ans.append([''.join(r) for r in board])
+                return
+
+            for col in range(n):
+
+                if col in cols:
+                    continue
+
+                if (row - col) in diag1:
+                    continue
+
+                if (row + col) in diag2:
+                    continue
+
+                board[row][col] = 'Q'
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+
+                board[row][col] = '.'
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+
+        return ans
+        
