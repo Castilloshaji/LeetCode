be694dc37974934f23b849a1660f0491d36cf8b4
Castillo Shaji <castilloshaji2005@gmail.com>
2026-08-05 23:39:56 +0530

    Add files via upload

---
+++ Intermediate/q81.py
@@ -0,0 +1,31 @@
+#5. Longest Palindromic Substring
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start = 0
+        max_len = 1
+
+        for i in range(len(s)):
+
+            left = right = i
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    max_len = right - left + 1
+                    start = left
+
+                left -= 1
+                right += 1
+
+            left = i
+            right = i + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    max_len = right - left + 1
+                    start = left
+
+                left -= 1
+                right += 1
+
+        return s[start:start + max_len]
+        
